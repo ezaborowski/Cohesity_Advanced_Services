8ee47b84767c93ebf346bcada33cb7e107d99cf3
ezaborowski <46197191+ezaborowski@users.noreply.github.com>
2022-06-08 17:46:21 -0400

    Add files via upload

--- PowerShell/Memorial_Hermann_scripts/Three_Strike_Failure/three_strike_failure_objects.ps1
+++ PowerShell/Memorial_Hermann_scripts/Three_Strike_Failure/three_strike_failure_objects.ps1
@@ -14,7 +14,7 @@ param (
 $version = $PSVersionTable.PSVersion
 if($version.major -lt 5.1){
     write-host "Please upgrade the PowerShell Module to the current revision of 7.2.4 by running the following command from your PowerShell prompt:"
-    write-host "msiexec.exe /package PowerShell-7.2.4-win-x64.msi /quiet ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ADD_FILE_CONTEXT_MENU_RUNPOWERSHELL=1 REGISTER_MANIFEST=1 USE_MU=1 ENABLE_MU=1"
+    write-host "msiexec.exe /package PowerShell-7.2.4-win-x64.msi ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ADD_FILE_CONTEXT_MENU_RUNPOWERSHELL=1 REGISTER_MANIFEST=1 USE_MU=1 ENABLE_MU=1"
 }
 else {
     write-host "PowerShell Module is up to date."
@@ -33,13 +33,26 @@ if($useApiKey){
 ### Get the Cluster Information
 $clusterdetails = api get /public/cluster
 
-### Get the End Date
-$enddate = Get-Date (Get-Date).ToUniversalTime() -UFormat %s
-$endtimeusecs = $enddate.PadRight(16,'0')
+function Get-CurrentUnixTimeStamp {
+    [DateTime]$epoch = New-Object System.DateTime 1970, 1, 1, 0, 0, 0, 0, Utc
+    [TimeSpan]$diff  = (Get-Date).ToUniversalTime() - $epoch
+    return [int64][Math]::Floor($diff.TotalSeconds)
+}
 
-### Get the Start Date
-$startdate = Get-Date (Get-Date).ToUniversalTime().AddDays(-3) -UFormat %s 
-$starttimeusecs = $startdate.PadRight(16,'0')
+[string]$currentTimeEpoch = Get-CurrentUnixTimeStamp
+$microDay = 86400000000
+
+### Get the End Date (Updated for legacy PowerShell terminal)
+# $enddate = Get-Date (Get-Date).ToUniversalTime() -UFormat %s
+# $endtimeusecs = $enddate.PadRight(16,'0')
+[string]$currentTimeEpoch = Get-CurrentUnixTimeStamp
+$currentDateEpoch = $currentTimeEpoch.PadRight(16,'0')
+$currentDateEpoch = $endtimeusecs
+
+### Get the Start Date (Updated for legacy PowerShell terminal)
+# $startdate = Get-Date (Get-Date).ToUniversalTime().AddDays(-1) -UFormat %s 
+# $starttimeusecs = $startdate.PadRight(16,'0')
+$starttimeusecs = ($currentDateEpoch - ($microDay * 3))
 
 ### Get the Failed Object Details
 $failedobjects = api get /public/reports/protectionSourcesJobsSummary?allUnderHierarchy=true`&endTimeUsecs=$endtimeusecs`&reportType=kFailedObjectsReport`&startTimeUsecs=$starttimeusecs`&statuses=kError
